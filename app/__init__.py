39d9fd725ca54bcdd6236a96ce8517e9ecb4def4
timothy <timlubanga@gmail.com>
2019-01-11 00:09:56 +0300

    fix failling tests

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
    app = Flask(__name__)
-   from .api.v1 import version1 as v1 
+   from api.v1 import version1 as v1 
    app.register_blueprint(v1)
    return app
 
