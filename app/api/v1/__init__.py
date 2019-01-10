90dbb30657d5a68b29b5f04a12f2ae336cf9e012
timothy <timlubanga@gmail.com>
2019-01-10 03:30:05 +0300

    [#finishes 163055559] create a question record

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,8 +2,10 @@ from flask_restful import Api
 from flask import Blueprint
 
 
-from views import Users,Meetups
+from views import Users,Meetups,Questions
 version1=Blueprint("api",__name__, url_prefix="/api/v1")
 api=Api(version1)
 api.add_resource(Users,'/<username>')
 api.add_resource(Meetups,'/meetup/<topic_name>')
+api.add_resource(Questions,'/question/<title_name>')
+
