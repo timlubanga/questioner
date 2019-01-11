c8a20c30fc0330e9bef63611a6cb57f5aa456c96
timothy <timlubanga@gmail.com>
2019-01-11 05:21:25 +0300

    fix failing tests

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from flask import Blueprint
 
 
-from views import Users,Meetups,Questions
+from .views import Users,Meetups,Questions
 version1=Blueprint("api",__name__, url_prefix="/api/v1")
 api=Api(version1)
 api.add_resource(Users,'/<username>')
